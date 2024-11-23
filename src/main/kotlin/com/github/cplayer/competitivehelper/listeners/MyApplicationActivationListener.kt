098a2a30e3eb1dcc795e6d40deb74c2d63231185
GitHub Action <action@github.com>
2024-11-23 18:55:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/cplayer/competitivehelper/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/cplayer/competitivehelper/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cplayer.competitivehelper.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
